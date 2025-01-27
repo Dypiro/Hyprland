07cbefb35a419d0e50fa86a4d109b2d71faa9735
Dypiro <131903899+Dypiro@users.noreply.github.com>
2025-01-27 18:53:40 +0100

    Create nvidiahybrid.sh

---
+++ nvidiahybrid.sh
@@ -0,0 +1,6 @@
+#run this after set-hypr and have relogged afterwards
+sudo pacman -Sy nvidia
+sudo pacman -Sy nvidia-utils
+sudo pacman -Sy nvidia-settings
+sudo pacman -Sy cuda
+sudo pacman -Sy nvidia-prime
